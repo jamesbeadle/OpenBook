f60040fef4058d69255400e9b584e329ee723851
James Beadle <jamesbeadle1989@gmail.com>
2023-10-31 08:57:50 +0000

    Juno site created

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onMount } from "svelte";
+    import { initJuno } from "@junobuild/core";
+  
+    // TODO: Replace 'satelliteId' with your actual satellite ID
+    onMount(
+      async () =>
+        await initJuno({
+            satelliteId: "aedk2-5yaaa-aaaal-adbra-cai",
+        })
+    );
+  </script>
+  
+  <slot />
