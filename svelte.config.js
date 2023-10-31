d827c801862bae6ba6374c7e2a4eeadecada15d7
James Beadle <jamesbeadle1989@gmail.com>
2023-10-31 09:18:02 +0000

    Update setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import autoprefixer from 'autoprefixer';
+const filesPath = (path) => `${path}`;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: vitePreprocess({
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	}),
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		files: {
+			assets: filesPath('static'),
+			lib: filesPath('src/lib'),
+			routes: filesPath('src/routes'),
+			appTemplate: filesPath('src/app.html')
+		}
 	}
 };
 
