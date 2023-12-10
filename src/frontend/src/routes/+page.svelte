0937a09313c6837febf13ab793c1dab9d80f7bd1
James Beadle <jamesbeadle1989@gmail.com>
2023-12-10 10:13:07 +0000

    Update landing

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import Layout from './Layout.svelte';
   import { Spinner } from '@dfinity/gix-components';
+  import Logo from '$lib/icons/logo.svelte';
 
   let isLoggedIn = false;
   let isLoading = true;
@@ -20,6 +21,25 @@
   {#if isLoading}
     <Spinner />
   {:else}
-    <h1>Welcome to OpenBook</h1>
+
+
+  <div class="flex flex-col md:flex-row h-screen">
+    <div class="relative md:w-1/2 w-full h-full overflow-hidden md:overflow-visible">
+      <img src="home.png" alt="home" class="md:absolute md:right-0 h-full md:w-full w-full object-cover md:object-none object-middle" />    </div>
+    <div class="md:flex-1 flex flex-col justify-center items-center p-4 md:p-12 mb-64 mt-64 md:mb-0 md:mt-0 space-y-4">
+      <Logo className="w-24" />
+      <p>Welcome Back</p>
+      <p>Please connect to continue</p>
+      <button>Connect</button>
+      <button>Whitepaper</button>
+      <p class="text-center">
+        Welcome to OpenBook, the future of accounting at your fingertips! 
+        Experience the ease of decentralised financial management, secure transaction management and real-time insights.
+        Simplify your accounting journey with OpenBook!
+      </p>
+  </div>
+</div>
+
+    
   {/if}
 </Layout>
