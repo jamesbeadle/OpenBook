43c03832876e20aeea639d06d602fbce3e458595
James Beadle <jamesbeadle1989@gmail.com>
2023-12-10 23:08:16 +0000

    Update roadmap text

--- src/frontend/src/lib/components/whitepaper/roadmap.svelte
+++ src/frontend/src/lib/components/whitepaper/roadmap.svelte
@@ -1,44 +1,80 @@
 <h1 class="text-2xl">Roadmap</h1>
 <div class="flex flex-col space-y-4 mt-4">
-    <p>OpenBook Future Features
-        As stated initially, OpenBook will venture into areas like Timesheet Management, Project Management,
-        Customer Service and more. These features will integrate seamlessly with the existing core services of
-        Finance, Sales, and HR.
-        Innovative Tax Reporting
-        A priority of the OpenBook team will be the development of global tax reports as templates, which will
-        evolve with domain-specific insights from experts.
-        Secure But Transparent Data
-        At OpenBook we understand the delicate balance between security and transparency in data handling.
-        Our approach is to provide external users with secure access to necessary data, maintaining privacy
-        while ensuring transparency. The will enable high-level financial reports to be made public after an
-        accounting period closure, maintaining detailed transaction confidentiality.
-        Road Map
-        November 2023: Development begins
-        April 2024: MVP Launch
-        September 2024: SNS Launch
-        January 2025: Joint Venture Accounting, Sales CRM & HR Platform Features Launch</p>
+    <p>Roadmap: Building Towards AI-Enhanced Business Operations</p>
+
+    <p>Ongoing: Security and Transparency Balance</p>
+    <ul>
+        <li>Maintaining the delicate balance between data security and transparency.</li>
+        <li>Ensuring private data remains protected while providing necessary insights to stakeholders.</li>
+    </ul>
+    
+    <p>Initial Phase: Establishing Core On-Chain Services</p>
+
+    <p>
+        2024: Launch of OpenBook MVP
+    </p>
+
+    <ul>
+        <li>Introduction of fundamental business management services: Finance, Sales, HR.</li>
+        <li>Seamless integration of these core services to establish the primary data lake.</li>
+    </ul>
+    
+    <p>
+        Late 2024: Expansion of Core Features
+    </p>
+
+    <ul>
+        <li>Adding Joint Venture Accounting, Timesheet Management, Project Management, and Customer Service.</li>
+        <li>Each new feature designed to enrich the data lake with diverse business operation insights.</li>
+    </ul>
+
+    <p>
+        Intermediate Phase: Broadening the Scope
+    </p>
+    
+    <p>
+        2025: Enriching the Platform
+    </p>
+
+    <ul>
+        <li>Integration of Inventory & Supply Chain Management, Risk Management, Recruitment, and Training.</li>
+        <li>Focus on Advanced Reporting features for more in-depth data analysis capabilities.</li>
+        <li>Implementation of global tax reports as templates, evolving with input from finance professionals.</li>
+    </ul>
+    
+
+    <p>Advanced Phase: Leveraging AI for Enhanced Business Insights</p>
+
+    <p>Post-2025: Introduction of AI Tools</p>
+
+    <ul>
+        <li>Utilizing the comprehensive data lake for AI-driven insights.</li>
+        <li>Democratically decided AI tool development through DAO voting, based on accumulated data and expert advice.</li>
+        <li>AI tools aimed at improving financial forecasting, operational efficiency, and strategic decision-making.</li>
+    </ul>
+
+
+    <p>Advanced Phase: Integrating Recruitment and Career Management</p>
+    
+    <ul>
+        <li>
+            2026 and Beyond: Launch of Business Profile and Recruitment Features
+        </li>
+        <li>
+            Introduction of a comprehensive business profile platform, aiming to rival established networks like LinkedIn.
+        </li>
+        <li>
+            Seamless integration of recruitment, career achievements, and professional networking into OpenBook.
+        </li>
+        <li>
+            Utilization of the data lake to provide AI-driven insights for career development and talent acquisition.
+        </li>
+    </ul>
+    
+    <p>Long-Term Vision: A Comprehensive Business Ecosystem</p>
+    
+    <p>OpenBook will evolve focused on transforming the landscape of business operations, from foundational business services, advanced AI-driven tools and professional networking.
+    </p>
+
 </div>
 
-
-
-<p>OpenBook is a secure, decentralised, multi-tenant, multi-currency role based platform designed to
-    ensure your business operates effectively 100% on-chain. With the ability to provide transparency to
-    current & future stakeholders in real time, OpenBook plans to change the way small businesses operate
-    from obtaining funding to rewarding employees.</p>
-<p>OpenBook plans to target the core business management SaaS platforms that exist within web2, initially
-            bringing these core services on-chain:</p>
-<p>- Finance
-                - Sales
-                - HR</p>
-<p>As the platform grows we plan to enhance the user experience for organisations by adding features for:</p>
-<p>
-                    - Joint Venture Accountancy
-- Timesheet Management
-- Project Management
-- Customer Service
-- Inventory & Supply Chain Management
-- Risk Management
-- Recruitment
-- Training
-- Advanced Reporting
-                </p>
